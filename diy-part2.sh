2f5b7a3c7d18adead1492714d7b641e860007098
Doanduy09 <96837081+Doanduy09@users.noreply.github.com>
2024-09-14 23:37:19 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,17 +13,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 sed -i '/CONFIG_PACKAGE_glib2=y/d' .config
-#echo 'CONFIG_FEED_atinout=y' >>.config
-#echo 'CONFIG_PACKAGE_luci-app-atinout-mod=y' >>.config
- cd feeds/luci/applications/
-git clone https://github.com/4IceG/luci-app-3ginfo-lite.git
-git clone https://github.com/4IceG/luci-app-atcommands.git
-git clone https://github.com/4IceG/luci-app-modemband.git
-git clone https://github.com/4IceG/luci-app-sms-tool.git
-cd /feeds/luci/themes/
-git clone https://github.com/jerrykuku/luci-theme-argon.git
-cd ../../..
-./scripts/feeds update -a && ./scripts/feeds install -a
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/msd_lite
 rm -rf feeds/packages/net/smartdns
